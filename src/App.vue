c6dfe1e1edfb4439b18ed9ae35a03c0b5570f014
Lyndon Farr <lyndonfarr@protonmail.com>
2022-02-28 09:53:59 -0300

    finito

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,106 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="form-group">
+      <label>Username</label>
+      <input v-model="user.username" type="text" class="form-control">
+    </div>
+    <div class="form-group">
+      <label>Mail</label>
+      <input v-model="user.email" type="text" class="form-control">
+    </div>
+    <button @click="submit" class="btn btn-primary">Submit</button>
+    <hr>
+    <input type="text" class="form-control" v-model="node">
+    <br><br>
+    <button class="btn btn-primary" @click="fetchData">Get Data</button>
+    <ul class="list-group">
+      <li :key="u.id" class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      user: {
+        username: '',
+        email: '',
+      },
+      users: [],
+      resource: {},
+      node: `data`,
+    }
+  },
+
+  created() {
+    const customActions = {
+      saveAlt: {
+        method: `POST`,
+        url: `alternative.json`,
+      },
+      getData: {
+        method: `GET`,
+      }
+    }
+    this.resource = this.$resource(`{node}.json`, {}, customActions)
+  },
+
+  methods: {
+    submit() {
+      // this.$http
+      //   .post(`data.json`, this.user)
+      //   .then(response => {
+      //     console.log(response)
+      //   }, error => {
+      //     console.log(error)
+      //   });
+      // this.resource.save({}, this.user);
+      this.resource.saveAlt(this.user);
+    },
+    fetchData() {
+      // this.$http
+      //   .get(`data.json`)
+      //   .then(response => {
+      //     return response.json();
+      //   })
+      //   .then(data => {
+      //     let resultArray = [];
+      //     Object.entries(data)
+      //       .forEach(entry => {
+      //         resultArray.push({
+      //           id: entry[0],
+      //           ...entry[1],
+      //         })
+      //       });
+      //     this.users = resultArray;
+      //   });
+      this.resource.getData({node: this.node})
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          // console.log(data);
+          // let resultArray = [];
+          // Object.entries(data)
+          //   .forEach(entry => {
+          //     resultArray.push({
+          //       id: entry[0],
+          //       ...entry[1],
+          //     })
+          //   });
+          // this.users = resultArray;
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
+    },
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource);
+
+Vue.http.options.root = `FIREBASE_PROJECT_URL`;
+Vue.http.interceptors.push((request, next) => {
+  if (request.method == `POST`) {
+    request.method = `PUT`;
+  }
+  next(response => {
+    response.json = () => {
+      return {
+        messages: response.body, 
+      }
+    }
+  });
+});
 
 Vue.config.productionTip = false
 
